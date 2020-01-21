e363fb524b19091a012b52c4b0f67260168725e2
Reuben Dunn <reuben.dunn@hypr.co.nz>
2020-01-21 16:34:05 +1300

    initial artifactory implementation

---
+++ artifactory/main.tf
@@ -0,0 +1,11 @@
+locals {
+  bucket_name         = "${format("%s-%s.%s", var.site_name, "artifactory", var.namespace)}"
+}
+resource "aws_s3_bucket" "artifactory" {
+  bucket = "${local.bucket_name}"
+  acl    = "private"
+
+  force_destroy = "${var.force_destroy}"
+
+  tags = "${merge(map("Name", local.bucket_name), var.tags)}"
+}
---
+++ artifactory/vars.tf
@@ -0,0 +1,19 @@
+variable "site_name" {
+  description = "The name of the static website or js bundle to be hosted by the S3 bucket. This will be the bucket name prefix."
+}
+
+variable "namespace" {
+  description = "A namespace that is appended to the `site_name-artifactory.` variable. This minimises S3 bucket naming collisions."
+}
+
+variable "force_destroy" {
+  description = "Controls if all objects in a bucket should be deleted when destroying the bucket resource. If set to `false`, the bucket resouce cannot be destroyed unitl all objects are deleted. Defaults to `true`."
+  default     = true
+}
+
+variable "tags" {
+  description = "A map of additional tags to add to the artifactory resource. A name tag with the value `<site_name>-artifactory.<namespace>` is added by default."
+  default     = {}
+}
+
+
