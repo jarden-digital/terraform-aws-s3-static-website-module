0274c28c744b13e78bc99c183778b9cdce9d4e1f
Reuben Dunn <reuben.dunn@hypr.co.nz>
2020-01-22 12:57:05 +1300

    Added Terraform 0.12 support for the artifactory module

--- artifactory/main.tf
+++ artifactory/main.tf
@@ -1,12 +1,12 @@
 locals {
-  bucket_name = "${format("%s-%s.%s", var.site_name, "artifactory", var.namespace)}"
+  bucket_name = format("%s-%s.%s", var.site_name, "artifactory", var.namespace)
 }
 
 resource "aws_s3_bucket" "artifactory" {
-  bucket = "${local.bucket_name}"
+  bucket = local.bucket_name
   acl    = "private"
 
-  force_destroy = "${var.force_destroy}"
+  force_destroy = var.force_destroy
 
-  tags = "${merge(map("Name", local.bucket_name), var.tags)}"
+  tags = merge(map("Name", local.bucket_name), map("Site Name", var.site_name), var.tags)
 }
